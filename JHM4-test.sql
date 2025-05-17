b0a544fd14ba3fb081af293aa994e2c7da55ec2a
wallace <s20017@ypicallmc.edu.hk>
2025-05-17 05:01:31 +0000

    update

---
+++ JHM4-test.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE JHM4_test;
+USe JHM4_test;
+CREATE Table STUDENT(
+    SID VARCHAR(10) PRIMARY KEY,
+    SNAME VARCHAR (50),
+    SEX  CHAR (1),
+    DOB  DATE,
+    CLASS VARCHAR(10)
+    );
+
+    INSERT INTO STUDENT (SID,SNAME,SEX,DOB,CLASS) VALUES
+    ('S001','陳大文','M' ,'2006-02-15' ,'4A'),
+    ('S002','李小明', 'M' ,'2005-11-23' ,'4B'),
+    ('S003','王美麗','F' ,'2006-05-30' ,'4A');
